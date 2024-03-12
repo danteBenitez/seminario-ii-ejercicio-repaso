bb221c4c60471d7c7ca34e900f383f059c658ba7
danteBenitez <benitezdante123@gmail.com>
2024-03-12 16:00:42 -0300

    feat: Botón e input funcionando

---
+++ index.js
@@ -0,0 +1,36 @@
+// Escoger elementos del DOM y guardarlos como variables
+const label = document.getElementById("label");
+const input = document.getElementById("input");
+
+// Estilar el label
+label.style.backgroundColor = 'yellow';
+label.style.color = 'red';
+label.style.display = 'none'; // Oculto por defecto hasta pulsar el botón
+label.style.maxWidth = '10rem';
+
+/**
+ * Función a ejectuar cuando se presiona el botón
+ * Cambia el color del label a rojo, realiza una alerta
+ * e imprime a la consola
+ */
+function execute(text) {
+  if (text.length == 0) {
+    label.style.display = "block";
+  } else {
+    label.style.display = "none";
+    const message = `Usted cargó ${text}`;
+    alert(message)
+    console.log(message);
+  }
+}
+
+// Registrar manejador de eventos para el botón
+document.getElementById("button").addEventListener("click", () => {
+  execute(input.value);
+});
+
+input.addEventListener("input", () => {
+  if (input.value.length != 0) {
+    label.style.display = "none";
+  }
+});
